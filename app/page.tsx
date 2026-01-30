2f23f18c69df6b76f1eba73dd216f2f53aee1156
MohamedSamirMoussa <mosamir5000@gmail.com>
2026-01-30 14:13:38 +0200

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,27 @@
-"use client"
+"use client";
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+ // Recommended: typed hooks
 import { refreshAuthThunk } from "./libs/redux/features/authSlice";
+import { useDispatch } from "react-redux";
+
+// Components
 const Home = React.lazy(() => import("./Components/Home/Home"));
 const About = React.lazy(() => import("./Components/About/About"));
-const OptionalMods = React.lazy(
-  () => import("./Components/OptionalMods/OptionalMods"),
-);
+const OptionalMods = React.lazy(() => import("./Components/OptionalMods/OptionalMods"));
 const Community = React.lazy(() => import("./Components/Community/Community"));
-const DiscordChannel = React.lazy(
-  () => import("./Components/DiscordChannel/DiscordChannel"),
-);
-const Main = () => {
-  const dispatch = useDispatch()
+const DiscordChannel = React.lazy(() => import("./Components/DiscordChannel/DiscordChannel"));
 
-useEffect(() => {
-  // محاولة استعادة الجلسة عند فتح الموقع
-  dispatch(refreshAuthThunk() as any);
-}, [dispatch]);
+const Main = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Attempt to restore session on mount
+    dispatch(refreshAuthThunk());
+  }, [dispatch]);
 
   return (
-    <React.Suspense fallback={null}>
+    /* Consider adding a loading spinner or skeleton in fallback */
+    <React.Suspense fallback={<div className="loading-screen" />}>
       <Home />
       <About />
       <OptionalMods />
